5a7b9204f6490211dd30c82f2229211ecb6edb6c
iamraafi <abdul_bandrang@dlsu.edu.ph>
2018-07-27 20:46:40 +0800

    attempt to fix no 4

--- MP1/Vector.java
+++ MP1/Vector.java
@@ -70,31 +70,52 @@ public class Vector{
 	}
 	public static int span (List<Vector> vectors, int dimension){
 		int span=0;
+		
 		for(int i=0;i<dimension;i++){
 			Vector curr=vectors.get(i);
-			curr=curr.scale(1/curr.vector[i]);
-			vectors.set(i, curr);
-			for(int k=i+1;k<dimension;k++){
-				Vector tempcurr=new Vector(curr.vector,dimension);
-				Vector temp=vectors.get(k);
-				double scalesize=temp.vector[i]*(-1);
-				vectors.set(k, temp.add(tempcurr.scale(scalesize)));
+			int j=0;
+			while(j<curr.dimension){
+				if(curr.vector[j]!=0)
+					break;
+				
+				j++;
 			}
-		}
-		for(int i=dimension-1;i>=0;i--){
-			Vector curr=vectors.get(i);
-			curr=curr.scale(1/curr.vector[i]);
-			vectors.set(i, curr);
-			for(int k=i-1;k>=0;k--){
-				Vector tempcurr=new Vector(curr.vector,dimension);
-				Vector temp=vectors.get(k);
-				double scalesize=temp.vector[i]*(-1);
-				vectors.set(k, temp.add(tempcurr.scale(scalesize)));
+//			System.out.println(curr.vector[0]+" "+curr.vector[1]+" "+curr.vector[2]+" "+j);
+//			System.out.println();
+			
+
+			if(j<curr.dimension){
+				curr=curr.scale(1/curr.vector[j]);
+				vectors.set(i, curr);
+				for(int k=i+1;k<dimension;k++){
+					Vector tempcurr=new Vector(curr.vector,curr.dimension);
+					Vector temp=vectors.get(k);
+					double scalesize=temp.vector[j]*(-1);
+					vectors.set(k, temp.add(tempcurr.scale(scalesize)));
+				}
 			}
+//			for(int a=0;a<dimension;a++){
+//				Vector currt=vectors.get(a);
+//				System.out.println(currt.vector[0]+" "+currt.vector[1]+" "+currt.vector[2]);
+//				
+//			}
+//			System.out.println();
+			
 		}
+//		for(int i=dimension-1;i>=0;i--){
+//			Vector curr=vectors.get(i);
+//			curr=curr.scale(1/curr.vector[i]);
+//			vectors.set(i, curr);
+//			for(int k=i-1;k>=0;k--){
+//				Vector tempcurr=new Vector(curr.vector,dimension);
+//				Vector temp=vectors.get(k);
+//				double scalesize=temp.vector[i]*(-1);
+//				vectors.set(k, temp.add(tempcurr.scale(scalesize)));
+//			}
+//		}
 		for(int i=0;i<dimension;i++){
 			Vector curr=vectors.get(i);
-			for(int k=0;k<dimension;k++){
+			for(int k=0;k<curr.dimension;k++){
 				if(curr.vector[k]>0){
 					span++;
 					break;
