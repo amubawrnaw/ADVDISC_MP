2101f973a31d42f4730ffaa304ceed67e8a4c756
amubawrnaw <amubawrnaw@gmail.com>
2018-07-30 00:38:14 +0800

    drayber

--- MP1/MC01.java
+++ MP1/MC01.java
@@ -3,5 +3,47 @@ import java.util.*;
 public class MC01{
 	public static void main(String[] args){
 		//Driver class for Vector
+
+		//input format: 
+		//n m
+		//n x m matrix
+		//1 x n matrix ( constants )
+
+		//sample input:
+		//2 3
+		//4 2 0
+		//6 9 6
+		//1 2 3
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		ArrayList<Vector> list = new ArrayList<>();
+
+		for(int i = 0 ; i < n ; i++){
+			double[] temp = new double[m];
+			for(int k = 0 ; k < m ; k++){
+				temp[k] = sc.nextInt();
+			}
+			Vector v = new Vector(temp, m);
+			list.add(v);
+		}
+
+		double[] temp = new double[n];
+		for(int k = 0 ; k < n ; k++){
+			temp[k] = sc.nextInt();
+		}
+		Vector constants = new Vector(temp, n);
+
+		Vector new_constants = Vector.Gauss_Jordan(list, m, constants);
+		int span = Vector.span(list, m);
+		String s = "Constants : ";
+		for(int i = 0 ; i < new_constants.dimension ; i++){
+			s+= new_constants.vector[i] + " ";
+		}
+		s+="Span : " + span;
+		System.out.println(s);
+
+
 	}
 }
